b76fd2be2e2ce60f2ad1cec5307cad2560330837
Onn Bii <onnbii933@gmail.com>
2026-07-20 01:41:29 +0200

    Implement login function in auth.js

---
+++ auth.js
@@ -0,0 +1,18 @@
+import api from "./api";
+
+export const login = async (
+  email,
+  password
+) => {
+
+  const response =
+    await api.post(
+      "/login",
+      {
+        email,
+        password
+      }
+    );
+
+  return response.data;
+};
