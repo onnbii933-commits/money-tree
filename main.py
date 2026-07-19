c89cb02db53a06607cc01d9669eda8a57621076f
Onn Bii <onnbii933@gmail.com>
2026-07-20 01:36:21 +0200

    Refactor FastAPI app to use routers for modularity

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
 from fastapi import FastAPI
 
-from mt5_connector import MT5Connector
+from routes.auth import router as auth_router
+from routes.trades import router as trades_router
+from routes.bots import router as bots_router
+
+from websocket import router as ws_router
 
 app = FastAPI()
 
-mt5 = MT5Connector()
+app.include_router(auth_router)
+
+app.include_router(trades_router)
+
+app.include_router(bots_router)
+
+app.include_router(ws_router)
 
 @app.get("/")
 def root():
 
     return {
-        "status": "online"
+        "status":
+        "online"
     }
-
-@app.get("/account")
-def account():
-
-    mt5.connect()
-
-    info = mt5.account_info()
-
-    return {
-        "login": info.login,
-        "balance": info.balance,
-        "equity": info.equity
-    }
-
-@app.get("/price/{symbol}")
-def price(symbol):
-
-    mt5.connect()
-
-    return mt5.symbol_price(
-        symbol
-    )
